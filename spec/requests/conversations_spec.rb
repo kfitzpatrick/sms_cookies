cd851392214feed2a0c019190dc62b44ad9c463f
Kevin Fitzpatrick <kfitzpatrick@KFITZPATRICK-3.local>
2013-09-19 14:13:51 -0700

    Switch to a spec style

--- spec/requests/conversations_spec.rb
+++ spec/requests/conversations_spec.rb
@@ -5,17 +5,16 @@ require 'rack/test'
 
 require File.expand_path('../../../sms_cookies_app.rb', __FILE__)
 
-class MyTest < MiniTest::Unit::TestCase
+include Rack::Test::Methods
 
-  include Rack::Test::Methods
+def app
+  SmsCookiesApp
+end
 
-  def app
-    SmsCookiesApp
-  end
-
-  def test_hello_monkey
+describe 'GET /' do
+  it 'should have some text' do
     get '/'
     assert last_response.ok?
-    assert_includes last_response.body, 'app should make'
+    last_response.body.must_include 'app should make'
   end
 end
